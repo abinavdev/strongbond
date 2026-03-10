32d81885c4c3bf55a406e9bd9e10b9c74e9d575d
Abinavdev A D <abinavdevabinav@gmail.com>
2026-03-10 13:52:56 +0530

    ver3

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,6 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import ProjectDetail from "./pages/ProjectDetail";
 import NotFound from "./pages/NotFound";
-import { CursorFollower } from "@/components/CursorFollower";
 
 const queryClient = new QueryClient();
 
@@ -15,7 +14,6 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <CursorFollower />
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Index />} />
